ae2160cda493e45395cc740a34881bf3ae589239
Russell Vinegar <russell.vinegar@gov.bc.ca>
2023-11-25 11:22:50 -0800

    use print map page

--- capture-screenshot.js
+++ capture-screenshot.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 
+// note - darkMode is demised and no longer does anything
 async function captureScreenshot(url, darkMode) {
     const browser = await puppeteer.launch({
         headless: "new"
@@ -27,56 +28,6 @@ async function captureScreenshot(url, darkMode) {
         }
         await waitTillHTMLRendered(page)
         console.log(`Capturing screenshot - dark mode? ${darkMode}`)
-        if (darkMode === true) {
-            {
-                const targetPage = page;
-                await puppeteer.Locator.race([
-                    targetPage.locator('div.map-settings > button'),
-                    targetPage.locator('::-p-xpath(/html/body/app-root/div/app-ski-area/div/div/div/npl-map-libre/div/div/div[3]/button)'),
-                    targetPage.locator(':scope >>> div.map-settings > button')
-                ])
-                    .setTimeout(timeout)
-                    .click({
-                      offset: {
-                        x: 15,
-                        y: 14,
-                      },
-                    });
-            }
-            {
-                const targetPage = page;
-                await puppeteer.Locator.race([
-                    targetPage.locator('::-p-aria(Dark) >>>> ::-p-aria([role=\\"image\\"])'),
-                    targetPage.locator('button:nth-of-type(2) > ion-img >>>> img'),
-                    targetPage.locator(':scope >>> button:nth-of-type(2) > ion-img >>>> :scope >>> img')
-                ])
-                    .setTimeout(timeout)
-                    .click({
-                      offset: {
-                        x: 31,
-                        y: 28,
-                      },
-                    });
-            }
-            {
-                const targetPage = page;
-                await puppeteer.Locator.race([
-                    targetPage.locator('div.map-settings > div > span'),
-                    targetPage.locator('::-p-xpath(/html/body/app-root/div/app-ski-area/div/div/div/npl-map-libre/div/div/div[3]/div/span)'),
-                    targetPage.locator(':scope >>> div.map-settings > div > span')
-                ])
-                    .setTimeout(timeout)
-                    .click({
-                      offset: {
-                        x: 13,
-                        y: 15,
-                      },
-                    });
-            }
-            await delay(2000);
-        }
-        console.log('Capturing screenshot')
-        // const map = await page.$('body > app-root > div > app-ski-area > div > div > div');
         const map = page
         // await new Promise(resolve => setTimeout(resolve, 5000)); // Wait for 5 seconds
         console.log('Returning screenshot')
